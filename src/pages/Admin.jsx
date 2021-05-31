344e8481e9d4e3123fb57fe3dff06faf47319402
Pablo Marino <pmarino2013@gmail.com>
2021-05-31 11:30:44 -0300

    Arreglando página de admin cuando no hay token generado

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -3,7 +3,7 @@ import jwt_decode from "jwt-decode"; //Paquete para decodificar el Token
 
 const Admin = () => {
   const [state, setState] = useState({});
-  const token = localStorage.getItem("token");
+  const token = JSON.parse(localStorage.getItem("token"));
   useEffect(() => {
     if (token) {
       let token_decode = jwt_decode(token); //Obteniendo los datos del payload del token
@@ -12,28 +12,43 @@ const Admin = () => {
     }
   }, [token]);
   return (
-    <div className="container mt-5">
-      <div className="row">
-        <div className="col">
-          <h1>Administrador</h1>
-          <hr />
-        </div>
-      </div>
-
-      <div className="row">
-        {state.role === "ADMIN_ROLE" ? (
-          <div className="col">
-            <h3>Bienvenido al ABM</h3>
-          </div>
-        ) : (
-          <div className="col">
-            <div className="alert alert-info" role="alert">
-              Lo sentimos, pero no tiene permisos para acceder a este contenido
+    <>
+      {token.length > 0 ? (
+        <div className="container mt-5">
+          <div className="row">
+            <div className="col">
+              <h1>Administrador</h1>
+              <hr />
             </div>
           </div>
-        )}
-      </div>
-    </div>
+
+          <div className="row">
+            {state.role === "ADMIN_ROLE" ? (
+              <div className="col">
+                <h3>Bienvenido al ABM</h3>
+              </div>
+            ) : (
+              <div className="col">
+                <div className="alert alert-info" role="alert">
+                  Lo sentimos, pero no tiene permisos para acceder a este
+                  contenido
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      ) : (
+        <div className="container mt-5">
+          <div className="row">
+            <div className="col">
+              <div className="alert alert-danger" role="alert">
+                No se encuentra logueado en la plataforma
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
